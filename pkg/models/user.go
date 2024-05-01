b0fea7fdbe1ebb04e9b74ea28f0a148cc5446ed8
Deatsilence <mert_im2000@hotmail.com>
2024-05-01 10:22:16 +0300

    Update password validation requirement to minimum 8 characters

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty"`
 	Name         *string            `json:"name" validate:"required,min=2,max=30"`
 	Surname      *string            `json:"surname" validate:"required,min=2,max=30"`
-	Password     *string            `json:"password" validate:"required,min=6"`
+	Password     *string            `json:"password" validate:"required,min=8"`
 	Email        *string            `json:"email" validate:"required,email"`
 	Token        *string            `json:"token"`
 	UserType     *string            `json:"usertype" validate:"required,eq=ADMIN|eq=USER"`
